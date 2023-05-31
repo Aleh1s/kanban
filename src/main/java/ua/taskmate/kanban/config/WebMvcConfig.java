fd7a0e6af8190b6926961e374c54e7182b7d469b
Oleksandr Palamarchuk <sashap740@gmail.com>
2023-05-31 10:26:07 +0300

    add PATCH method to cors registry

--- src/main/java/ua/taskmate/kanban/config/WebMvcConfig.java
+++ src/main/java/ua/taskmate/kanban/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
 }
